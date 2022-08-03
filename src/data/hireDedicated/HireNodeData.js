5df0c7210b1252d739244e6ac16a71d3917373a4
shirin fatema <shirinfatema666@gmail.com>
2022-08-03 16:54:39 +0530

    Added Node.js file

---
+++ src/data/hireDedicated/HireNodeData.js
@@ -0,0 +1,163 @@
+
+export const HireNodeData = {
+    metaTitle: "Hire Node.JS Developers | Hire Node.JS Developers",
+title: "Hire Node.JS ",
+    titleSpan: "Developers",
+    smallTitle: "Hire Node.JS developers trusted by 97% of CTO",
+    paragraph:
+      "Hire Node.JS developers with a proven track record of building Silicon Valley-like app development. Try for free Now!",
+    btnLink: "#contact-form-top",
+    btnText: "Get Rate Card in My Inbox",
+    imgSrc: "/images/NodeImages/hire-nodejs-developers-banner.webp",
+    imgAlt: "Hire Node.JS Developers",
+    clutchReviewText:
+      `"The team had very fast and effective<br/> communication."`,
+    listMenus: [
+      {
+        id: 1,
+        title: "Scalable",
+        imgSrc: "/images/PhpImages/Php-scalable.svg",
+      },
+      {
+        id: 2,
+        title: "Secure",
+        imgSrc: "/images/PhpImages/Php-secure.svg",
+      },
+      {
+        id: 3,
+        title: "User friendly",
+        imgSrc: "/images/HireDedicated/icons/user-friendly.svg",
+      },
+    ],
+    expertiseIns: {
+      title: "Hire Node Developers with Expertise in",
+      subTitle:
+        "Hire top-notch quality, pre-vetted, certified Node.JS programmers to work for your project from Today itself.",
+      expertLists: [
+        {
+          expertImage:
+            "/images/NodeImages/node-development.svg",
+          expertTitle: "Node.JS<br/>Development",
+          expertSubTitle:
+            "Hire dedicated Node.JS developers who are experts in providing a wide range of plugins, custom dashboards, real-time interactive lightweight applications as per your business requirements.",
+        },
+        {
+          expertImage:
+            "/images/NodeImages/node-integration.svg",
+          expertTitle: "API Development and<br/>Integration",
+          expertSubTitle:
+            "Develop and integrate APIs with our Node.JS programmers. Our Node.JS developers help you in developing Enterprise-grade REST APIs with smooth third-party integrations, matured manual as well as automated testing, all-level security configuration, and monitoring etc.",
+        },
+        {
+          expertImage:
+            "/images/NodeImages/node-plugin.svg",
+          expertTitle: "Node.JS Plugin<br/> Development",
+          expertSubTitle:
+            "Hire Node.JS experts to accelerate your application's performance with custom plugin development. They are well acquainted with recent updates and will certainly help you to get the best solution considering all the techno-commercial aspects of your requirements.",
+        },
+        {
+          expertImage:
+            "/images/NodeImages/node-maintenance.svg",
+          expertTitle: "Node.JS Support and<br/>Maintenance",
+          expertSubTitle:
+            "Our remote Node.JS developers are here to help you right from performance to bug fixes, improvements, and upgrades. We take care of every major of your Mobile App or Web Application to deliver you errorless, secured and seamless performance.",
+        },
+        {
+            btnLink: "#contact-form-top",
+            btnText: "Hire Node.JS Developers",
+  
+        },
+      ],
+    },
+    whyTechs: {
+      title: "eLuminous to Hire Node.JS<br/>Developers?",
+      subTitle: "Why Tech Managers opt",
+      paragraph:
+        "Hire Node.JS developers with flexible engagement models like full-time, part-time, or Adhoc basis as per your business need.",
+      imgSrc: "/images/NodeImages/hire-nodejs.webp",
+      imgAlt: "Laptop",
+    titleSpan:"Let’s innovate together.",
+    btnLink: "#contact-form-top",
+            btnText: "Hire Node.JS Developers",
+
+      WhyTechLists: [
+        {
+          title: "Trusted by People <br />Like You!",
+          icon: "/images/HireDedicated/icons/trusted-by-people-like-you.svg",
+          paragraph:
+            "Best UI Winner and many 5 Star Reviews on <b>Clutch, GoodFirms, Google.</b>",
+        },
+        {
+          title: "Quick<br/>Onboarding",
+          icon: "/images/PhpImages/php-onboarding.svg",
+          paragraph: "We have a straightforward process for hiring Node.JS developers for quick onboarding.",
+        },
+        {
+          title: "100% Tested<br/>Process",
+          icon: "/images/PhpImages/php-tested.svg",
+          paragraph:
+            "Hands-on experience in building rich applications. We use Rest APIs for integration with ERP/SAP.",
+        },
+        {
+          title: "SEO-friendly <br />Apps",
+          icon: "/images/HireDedicated/icons/seo-friendly-apps.svg",
+          paragraph:
+            "We create Websites indexable and visible to Search Engines. We deploy best code practices to develop Google-friendly apps. ",
+        },
+      ],
+    },
+    faqs: {
+      faqLists: [
+        {
+          id: "0",
+          title: "Why hire dedicated Node.js developers? ",
+          paragraph:
+            "<p>When you hire dedicated Node.js developers, you get complete access to your project work progress.PMP-certified dedicated<br/> project manager will be there to assist you and technical team for smooth project execution and delivery. This aims to faster<br/>time-to-marke</p>",
+          ulLists: [
+            {
+              list: "",
+            },
+          ],
+        },
+        {
+          id: "1",
+          title: "Do you offer support and maintenance service to clients? ",
+          paragraph:
+            "<p>Absolutely Yes. We do offer support and maintenance services to our clients in order to help you in acheiving your business goals. </p>",
+          
+        },
+{
+    id:"2",
+    title:"Is testing of the components included within the pricing structure? ",
+    paragraph:"As per benchmarked quality structure, testing is performed by the developers before the delivery of their work. If you want, you<br/>can also hire Quality Analyst for better testing.",
+   },
+
+{
+          id: "3",
+          title: "With Node.js, what kind of solutions can we create? ",
+          paragraph: "Our dedicated Node.JS developers can create web portals, games, chats, beck-end dashboards, real-time apps like social networking & many more.",
+          
+        },
+        {
+            id:"4",
+            title:"Why choose Node.js for software development requirements? ",
+            paragraph:"",
+           
+        ulLists: [
+            {
+              list: "Compatible: It is fully compatible with popular web browsers for both clients- and server-side code.",
+            },
+            {
+              list: "Scalable: It allows you to build scalable applications",
+            },
+            {
+              list: "Faster Time-to-Market",
+            },
+            
+        
+          ],
+        },
+      ],
+    },
+  };
+  
