ff0a8f80a1e467b22325b61cf1ebf85927711de0
shirin fatema <shirinfatema666@gmail.com>
2022-08-04 18:30:31 +0530

    Added Hire iOs file

---
+++ src/data/hireDedicated/iOsData.js
@@ -0,0 +1,221 @@
+export const HireiOsData = {
+    metaTitle: "Hire iOs Developers | Hire iOs Developers",
+  
+      title: "Hire iOs ",
+      titleSpan: "Developers",
+      smallTitle: "Turn Your Idea Into Reality with Best of Apple App Store Award Winners!",
+      paragraph:
+        "Hire iOS developers with great business acumen to deliver iOS Apple App Store Award-winning capable apps to You.",
+      btnLink: "#contact-form-top",
+      btnText: "Get Rate Card in My Inbox",
+      imgSrc: "/images/iOsImages/hire-ios-developers-banner.webp",
+      imgAlt: "Hire iOs Developers",
+      clutchReviewText:
+        `"Their team does their research and never says no, which empowers us."`,
+        listMenus: [
+          {
+            id: 1,
+            title: "Scalable",
+            img: "/images/PhpImages/Php-scalable.svg",
+          },
+          {
+            id: 2,
+            title: "Secure",
+            img: "/images/PhpImages/Php-secure.svg",
+          },
+          {
+            id: 3,
+            title: "User friendly",
+            img: "/images/HireDedicated/icons/user-friendly.svg",
+          },
+        ],
+      expertiseIns: {
+        title: "Hire iOs Developers with Expertise in",
+        subTitle:
+          "Hire iOS developers to bring result-driven mobile apps for global clients with diverse domain expertise. Let's develop a feature-rich, scalable, secured, and interactive iOS app today!",
+        expertLists: [
+          {
+            expertImage:
+              "/images/iOsImages/ios-app.svg",
+            expertTitle: "Custom iOS<br/>App Development",
+            expertSubTitle:
+              "Our dedicated iOS app developers are skilled to help you in building custom iOS applications for iPhones acknowledged by Best of Apple App Store Award.",
+          },
+          
+          {
+            expertImage:
+              "/images/iOsImages/ios-uiUx.svg",
+            expertTitle: "iOS App<br/> UI/UX Design",
+            expertSubTitle:
+              "Being Best UI Award Winners, we are specialized in offering seamless visualizations, intuitive UI for iOS apps. The user-centered approach has been helping us to create interactive applications.",
+          },
+          {
+            expertImage:
+              "/images/iOsImages/iOs-watch.svg",
+            expertTitle: "Apple Watch/Apple TV<br/>App Developement",
+            expertSubTitle:
+              "Our iOS app development team does not limit your ideas to mobile phones. Turn your iOS apps into watch Apple watch apps, Apple TV app development like Netflix.",
+          },
+          {
+              expertImage:
+                "/images/iOsImages/ios-app-testing.svg",
+              expertTitle: "iOS App Testing &<br/>Upgradation",
+              expertSubTitle:
+                "Our aim is to provide bug-free iOS apps. We do manual and automated testing. Check functionality, accessibility, and usability before launching your app to the market. Get easy upgradations for multiple iOS OS here.",
+            },
+            {
+              btnLink: "#contact-form-top",
+               btnText: "Hire iOs Developer",
+            },
+        ],
+      },
+      whyTechs: {
+        title: "eLuminous to Hire iOs<br /> Developers?",
+        subTitle: "Why Tech Managers opt",
+        paragraph:
+          "Hire iOS app developers with flexible engagement models like full-time, part-time, or Adhoc basis as per your business need.",
+        imgSrc: "/images/iOsImages/hire-ios.webp",
+        imgAlt: "Laptop",
+        titleSpan:"Let’s innovate together.",
+        btnLink: "#contact-form-top",
+      btnText: "Hire iOs Developer",
+   
+  
+  
+        WhyTechLists: [
+          {
+            title: "Trusted by People <br />Like You!",
+            icon: "/images/HireDedicated/icons/trusted-by-people-like-you.svg",
+            paragraph:
+              "Best of UI Winner and many 5 Star Reviews on Clutch, GoodFirms and Google.",
+  
+  
+          },
+          {
+            title: "Quick<br/>Onboarding",
+            icon: "/images/PhpImages/php-onboarding.svg",
+            paragraph: "We have a straightforward process for hiring iOS application developers for quick onboarding.",
+          },
+          {
+            title: "Access to Cutting-Edge Technologies",
+            icon: "/images/iOsImages/ios-tech.svg",
+            paragraph:
+              "Hands-on experience in building IoT and AI-based mobile apps. We use Rest APIs for integration with ERP/SAP.",
+          },
+          {
+            title: "3-Months Post-Delivery Support",
+            icon: "/images/iOsImages/ios-support.svg",
+            paragraph:
+              "We promise free technical support of 3-Months after app delivery.",
+          },
+        ],
+      },
+portfolio:{
+    title:"Portfolio",
+    PortfolioList:[
+{
+    imgSrc:"/images/iOsImages/fakturakollen.webp",
+    imgAlt:"Fakturakollen",
+    title:"Fakturakollen",
+    paragraph:"App Development for keeping all your Invoices from<br/>emails at One Place with keyword mangement",
+},
+{
+    imgSrc:"/images/iOsImages/Puremed.webp",
+    imgAlt:"Puremed",
+    title:"Puremed",
+    paragraph:"IoT based User-Friendly App to track Medical<br/>Appointments & Tests",
+},
+{
+    imgSrc:"/images/iOsImages/Behaviour-Management-Analysis.webp",
+    imgAlt:"Behaviour Management Analysis",
+    title:"Behaviour Management Analysis",
+    paragraph:"App Development in Healthcare Industry -<br/>Behaviuor Analysis",
+},
+{
+    imgSrc:"/images/iOsImages/MustAToZ-Clothing.webp",
+    imgAlt:"MustAToZ Clothing",
+    title:"MustAToZ Clothing",
+    paragraph:"Mobile App Development for Online Store",
+},
+{
+      btnLink: "#contact-form-top",
+      btnText: "Hire iOs Developer",
+   
+},
+],
+
+
+},
+
+
+faqs: {
+        faqLists: [
+          {
+            id: "0",
+            title: "Which tools are used to develop iOS App? ",
+            paragraph:
+              "<p>Our dedicated iOS app developers use the following tools for iOS app development:</p>",
+            ulLists: [
+              {
+                list: "iOS Studio",
+              },
+              {
+                list: "iOS SDK",
+              },
+              {
+                list: "iOS NDK43. Instabug",
+              },
+              {
+                list: "iOS Debug Bridge",
+              },
+            ],
+          },
+          {
+            id: "1",
+            title: "Can you show me your previous work samples? ",
+            paragraph:
+              "<p>We have extensive expertise in iOS application development for distinct verticles.</p>",
+            ulLists: [
+              {
+                list: "Puremed",
+              },
+              {
+                list: "Fakturakollen",
+              },
+              {
+                list: "Behaviour Management Analysis",
+              },
+              {
+                list: "MustAToZ Clothing",
+              },
+              {
+              paragraph:"Check out our portfolio page to get to know in detail.",
+              },
+            ],
+          },
+  
+          {
+            id: "2",
+            title: "What if I need any modification or change in the mobile app after project completion? ",
+            paragraph:
+              "<p>Based on the type/format of change or modification you need, we will be happy to provide upcoming development.</p>",
+                    },
+          {
+            id: "3",
+            title: "Do you sign NDA? I want to keep My iOS App idea confidential. ",
+            paragraph: "Yes, of course. We respect your confidentiality. By signing a non-disclosure agreement with you, we keep shared ideas safe and secure with us.",
+
+     },
+  
+     {
+      id: "4",
+      title: "Will I own Source Code? ",
+  paragraph:"Though we develop the iOS app for you, App's source code belongs to you. The property rights are exclusively yours. "
+          
+  },
+  
+          
+        ],
+      },
+    };
+    
