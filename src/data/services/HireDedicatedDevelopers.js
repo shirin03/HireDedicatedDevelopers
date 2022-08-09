ca10c7a5a4a00c5500d9b2d56b409646638e1e25
shirin fatema <shirinfatema666@gmail.com>
2022-08-09 17:31:15 +0530

    Added HireDedicatedDeveloper file

---
+++ src/data/services/HireDedicatedDevelopers.js
@@ -0,0 +1,429 @@
+export const HireDedicatedDevelopers = {
+    metaTitle: "Hire Dedicated Developers | Hire Dedicated Developers",
+    metaDescription:
+      "Hire Dedicated Developers to have seamless visulizations across all devices and help you to build high performace scalable apps to enhance productivity by 70%.",
+    metaKeywords:
+      "hire dedicated developers, hire dedicated developers,dedicated  developers",
+    title: "Hire Dedicated ",
+    titleSpan: "DEVELOPERS",
+    smallTitle: "Agile, PMP Certified Dedicated Developers<br/>are here to help You!",
+    paragraph:
+      "Get access to the Top 1% of the talented development team elected to meet your project needs. Hire dedicated developers who are sourced and screened from extensive vetting process and ready to work for you from Today iteslf!",
+    btnLink: "#contact-form-top",
+    btnText: "Get Rate Card in My Inbox",
+    imgSrc: "/images/HireDedicatedImgs/hire-dedicated-developers.webp",
+    imgAlt: "Hire Dedicated Developers",
+    clutchReviewText:
+      `"I was really grateful that they stuck with me and ensured that it was delivered on time."`,
+    listMenus: [
+      {
+        id: 1,
+        img: "/images/HireDedicatedImgs/clutch.webp",
+        caption:"Clutch Rated Top<br/>100 IT Company",
+      },
+      {
+        id: 2,
+        
+        img: "/images/HireDedicatedImgs/dedicated-developers.webp",
+        caption:"130+ Dedicated Developers"
+      },
+      {
+        id: 3,
+        
+        img: "/images/HireDedicatedImgs/HireDedicated-client.svg",
+        caption:"900+ Happy<br/>Clients",
+      },
+    ],
+    DevelperWhoAre:{
+        imgSrc:"/images/HireDedicatedImgs/hire-dedicated-developers-who-are.webp",
+        title:"Hire Dedicated Developers Who Are",
+        imgList:[
+{
+    id:"1",
+    img:"/images/HireDedicated/icons/trusted-by-people-like-you.svg",
+    title:"Certified Developers - PMP, Agile, Laravel, Android",
+},
+{
+    id:"2",
+    img:"/images/HireDedicated/icons/tested-process.svg",
+    title:"Fast/Quick<br/>Onboarding",
+},
+{
+    id:"3",
+    img:"/images/HireDedicated/icons/tested-process.svg",
+    title:"Full Control Over<br/>Development",
+},
+{
+    id:"4",
+    img:"/images/HireDedicated/icons/seo-friendly-apps.svg",
+    title:"Daily/Weekly/Monthly Code Delivery",
+},
+],
+},
+
+scaleUp:{
+title:"Scale Up Your Team with as You Need",
+subtitle:"We offer a highly flexible and customized team augmentation services as per your business requirements.",
+imgList:[
+{
+    id:"1",
+    imgSrc:"/images/HireDedicated/icons/custom-angular-development.svg",
+    title:"Build Team<br/>from Scratch",
+    paragraph:"Build your own team with distinct skillsets to transform your business idea into product from scratch.",
+},
+{
+    id:"2",
+    imgSrc:"/images/HireDedicatedImgs/HireDedicated-team.svg",
+    title:"Easy Ramp Up and<br/>Ramp Down of Your Team",
+    paragraph:"Hire dedicated developers to quickly scale up the team and easily ramp down once your work gets done.",
+},
+{
+    id:"3",
+    imgSrc:"/images/HireDedicated/icons/reactjs-integration-and-migration-services.svg",
+    title:"Vendor<br/>Transition",
+    paragraph:"Having some issues with your current vendor? We are here to help your sinking midway project transition with Agile Transfomation.",
+},
+{
+        btnLink: "#contact-form-top",
+      btnText: "Hire Dedicated Developer",
+},
+
+
+]
+},
+
+TechnologyStack:{
+
+    title:"Hire Dedicated Developers with<br/>Technology Stack",
+    titleSpan:"<a href='https://hire-dedicated-developer/#pills-Web'>Web</a>",
+ulLists:[
+    {
+        id:"1",
+        logo:"/images/HireDedicatedImgs/AngularJS.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-angular-developers/'>Angular</a>",
+    },
+    {
+        id:"2",
+        logo:"/images/HireDedicatedImgs/VueJs.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-vuejs-developers/'>Vue Js</a>",
+    },
+    {
+        id:"3",
+        logo:"/images/HireDedicatedImgs/ReactJS.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-reactjs-developers/'>React Js</a>",
+    },
+    {
+        id:"4",
+        logo:"/images/HireDedicatedImgs/nuxt.webp",
+        caption:"Nuxt.Js",
+    },
+    {
+        id:"5",
+        logo:"/images/HireDedicatedImgs/Laravel.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-laravel-developers/'>Laravel</a>",
+    },
+    {
+        id:"6",
+        logo:"/images/HireDedicatedImgs/Node.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-nodejs-developers/'>Node</a>",
+    },
+    {
+        id:"7",
+        logo:"/images/HireDedicatedImgs/PHP.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-php-developers/'>PHP</a>",
+    },
+    {
+        id:"8",
+        logo:"/images/HireDedicatedImgs/net.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-dotnet-developers/'>.net</a>",
+    },
+    {
+        id:"9",
+        logo:"/images/HireDedicatedImgs/netCore.webp",
+        caption:".net core",
+    },
+    {
+        id:"10",
+        logo:"/images/HireDedicatedImgs/Python.webp",
+        caption:"Python",
+    },
+    {
+        id:"11",
+        logo:"/images/HireDedicatedImgs/Django.webp",
+        caption:"Django",
+    },
+],
+titleSpan:"<a href='https://hire-dedicated-developer/#pills-Mobile'>Mobile</a>",
+ulLists:[
+    {
+        id:"1",
+        logo:"/images/HireDedicatedImgs/Flutter.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-flutter-developers/'>Flutter</a>",
+    },
+    {
+        id:"2",
+        logo:"/images/HireDedicatedImgs/ReactNative.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-reactNative-developers/'>React Native</a>",
+    },
+    {
+        id:"3",
+        logo:"/images/HireDedicatedImgs/iOS.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-ios-developers/'>iOS</a>",
+    },
+    {
+        id:"4",
+        logo:"/images/HireDedicatedImgs/Android.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-android-developers/'>Android</a>",
+    },
+    {
+        id:"1",
+        logo:"/images/HireDedicatedImgs/Ionic.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-ionic-developers/'>Ionic</a>",
+    },
+],
+titleSpan:"<a href='https://hire-dedicated-developer/#pills-CMS'>CMS</a>",
+ulLists:[
+    {
+        id:"1",
+        logo:"/images/HireDedicatedImgs/WordPress.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-wordpress-developers/'>WordPress</a>",
+    },
+    {
+        id:"2",
+        logo:"/images/HireDedicatedImgs/Drupal.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-drupal-developers/'>Drupal</a>",
+    },
+    {
+        id:"3",
+        logo:"/images/HireDedicatedImgs/magento.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-magento-developers/'>Magento</a>",
+    },
+    {
+        id:"4",
+        logo:"/images/HireDedicatedImgs/shopify.webp",
+        caption:"<a href='https://hire-dedicated-developer/hire-shopify-developers/'>Shopify</a>",
+    },
+
+],
+titleSpan:"<a href='https://hire-dedicated-developer/#pills-Database'>Database</a>",
+ulLists:[
+    {
+        id:"1",
+        logo:"/images/HireDedicatedImgs/MongoDB.webp",
+        caption:"MongoDB",
+    },
+    {
+        id:"2",
+        logo:"/images/HireDedicatedImgs/PostgreSQL.webp",
+        caption:"PostgreSQL",
+    },
+    {
+        id:"3",
+        logo:"/images/HireDedicatedImgs/MySQL.webp",
+        caption:"MySQL",
+    },
+],
+titleSpan:"<a href='https://hire-dedicated-developer/#pills-Server'>Server</a>",
+ulLists:[
+    {
+        id:"1",
+        logo:"/images/HireDedicatedImgs/AWS.webp",
+        caption:"AWS",
+    },
+    {
+        id:"2",
+        logo:"/images/HireDedicatedImgs/DigitalOcean.webp",
+        caption:"Digital Ocean",
+    },
+    {
+        id:"3",
+        logo:"/images/HireDedicatedImgs/azure.webp",
+        caption:"Azure",
+    },
+
+    {
+        btnLink: "#contact-form-top",
+      btnText: "Get Started Today",
+},
+],
+},
+
+title:"Engagement Models for Hiring Dedicated Developers",
+paragraph:"Software outsourcing has never been so easy ! We can help you to hire skilled Developers, Testers, Business Analysts and Technology Architects as per your need in optimum time. This will help you to accelerate software development, reduce cost and nitty-gritties of hiring best talent.",
+ulLists:[
+{
+    id:"1",
+    imgSrc:"/images/HireDedicatedImgs/dedicated-developers.webp",
+    title:"Individual Developers/<br/>Testers",
+    paragraph:"",
+    ulLists:[
+        {
+            list:"Hire skilled Developers or testers who can operate remotely",
+        },
+        {
+            list:"Onsite developers or testers with relevant work culture awareness",
+        },
+        ]
+},
+{
+    id:"2",
+    imgSrc:"/images/HireDedicatedImgs/fully-functioinal-pods.webp",
+    title:"Fully Functional<br/> PODs",
+    paragraph:"",
+    ulLists:[
+        {
+            list:"Agile Development PODs with Technology Architect, Developers, Testers and Bas",
+        },
+        {
+            list:"Flexibility of designing a POD by selecting right roles",
+        },
+        ]
+},
+{
+    id:"3",
+    imgSrc:"/images/HireDedicated/icons/tested-process.svg",
+    title:"Pizza PODs",
+    paragraph:"",
+    ulLists:[
+        {
+            list:"Hire specialists in development or testing who will faciliate to your existing POD or software team",
+        },
+        {
+            list:"Flexibility of hiring specialists on need basis saves significant cost and reduce talent risk",
+        },
+        ]
+},
+{
+    btnLink: "#contact-form-top",
+  btnText: "Get Started Today",
+},
+
+],
+AboutELuminous:{
+    ulLists:[
+{
+    titleSpan:"Key Highlights",
+    textList:[
+        {
+            list:"19+ years<br/>of experience",
+        },
+        {
+            list:"600+<br/>Happy Client",
+        },
+        {
+            list:"130+<br/>IT Professionals",
+        },
+        {
+            list:"70%<br/>Client Retention Rate",
+        },
+    ]
+},
+{
+    titleSpan:"Recongnitions",
+    imgList:[
+        {
+            img:"/images/HireDedicatedImgs/clutch-icon.webp",
+        },
+        {
+            img:"/images/HireDedicatedImgs/Badge-Mobile-App-Developers-2020.webp",
+        },
+        {
+            img:"/images/HireDedicatedImgs/app-developers-usa-2020.png",
+        },
+        {
+            img:"/images/HireDedicatedImgs/goodfirms-blue.svg",
+        },
+    ]
+},
+{
+    titleSpan:"Our Esteemed Clients",
+    imgList:[
+        {
+            img:"/images/HireDedicatedImgs/razer.webp",
+        },
+        {
+            img:"/images/HireDedicatedImgs/viacom_18.webp",
+        },
+        {
+            img:"/images/HireDedicatedImgs/color.webp",
+        },
+        {
+            img:"/images/HireDedicatedImgs/sokule.webp",
+        },
+    ]
+
+}
+    ]
+
+},
+
+
+
+ faqs: {
+      faqLists: [
+        {
+          id: "0",
+          title: "Why hire dedicated developers instead of freelancers? ",
+          paragraph:
+            "<p>When you hire dedicated developers, what you get is comprehensive expertise in one place with reputation and maximum commitment. In the case of freelancing, there is a little bit of insecurity about above-mentioned points. With hire dedicated development model you can easily ramp-up and ramp down the team as per your project requirement.</p>",
+        
+        },
+        {
+          id: "1",
+          title: "How much does it cost to hire dedicated developers? ",
+          paragraph:
+            "<p>If you want to hire dedicated developers full-time, then it will cost you in between $2400 to $4000 per month which is subjective to the skillset and experience of the candidate.</p>",
+          
+        },
+        {
+          id: "2",
+          title: "What kind of company would use a dedicated team? ",
+          paragraph:
+            "<p> Companies with non-technical executives and have some limitations while managing and leading software development projects can hire dedicated developers to get their work done in systematic approach.</p>",
+          
+        },
+        {
+          id: "3",
+          title: "What are the benefits of hiring dedicated developers? ",
+          paragraph: "",
+          ulLists: [
+            {
+              list: "Save money and time",
+            },
+            {
+              list: "Improve productivity",
+            },
+            {
+              list: "IP security and ownership",
+            },
+            {
+              list: "Direct communication",
+            },
+            {
+              list: "Flexible hiring",
+            },
+            {
+              list:"Multiple skillsets",
+            },
+          ],
+        },
+        {
+            id:"4",
+            title:"What is your resource backup strategy? If my existing resource is unavailable due to some emergency or plans to change the organization?",
+            paragraph:"Don't worry. We have a pool of 130+ developers and we always take care of mandatory provisions for smooth project execution within committed time period.",
+        },
+        {
+            id:"5",
+            title:"What if I am not satisfied? ",
+            paragraph:"Be assured. It will never happen as we choose our resources carefully. Still, if you are unhappy we shall be sharing an escalation process. You can share your concern with our Account manager and Delivery managers. We shall replace the resource as soon as possible. Generally, when we onboard a new resource, our Seniors pay close attention and ensure you a timely and quality outcome. According to the business scenario or any reason all parties have the right to give 30 days' notice and opt for adding/removing the resources.",
+        },
+        {
+            id:"6",
+            title:"How do you do manage timezone difference in Outsourcing? ",
+            paragraph:"We provide time overlapping between clients' timezone (GMT/CST/PST/EST) and eLuminous teams. Time overlapping will allow smooth communication between stakeholders and assured quality work and on-time delivery.",
+        },
+
+      ],
+    },
+  };
+  
